fd8bdfa64692afe2141d43a207609ee4e1f4f83b
lokithe5th <lourenslinde@gmail.com>
2024-08-26 11:31:16 +0200

    Feat: init

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -9,16 +9,16 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+        counter.setNumber(0, false);
     }
 
-    function test_Increment() public {
+    function testIncrement() public {
         counter.increment();
-        assertEq(counter.number(), 1);
+        assert(counter.number() == 1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testSetNumber(uint256 x, bool inLuck) public {
+        counter.setNumber(x, inLuck);
+        assert(counter.number() == x);
     }
 }
